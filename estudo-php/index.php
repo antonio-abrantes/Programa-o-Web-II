1973781ab926ad15459c42b10a64684812fcec5d
tonhus <antonio_abrantes@hotmail.com.br>
2018-06-05 12:58:10 -0300

    commit estudo-php

---
+++ estudo-php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Principal</title>
+</head>
+<body>
+    <h1>Programação Web II - 2018</h1><hr>
+    <div id="lista-aulas">
+        <a href="aula-01.html">Aula 01</a>
+    </div>
+</body>
+</html>
+
+<?php 
+    
+?>
